4ca2823173de4d1f657d02fff1e7e4562a4429a0
hyunsoo <mm083434@gmail.com>
2025-06-09 20:52:04 +0900

    chore: project setup and configure KakaoTalk login

--- Sweav/App/SweavApp.swift
+++ Sweav/App/SweavApp.swift
@@ -1,7 +1,14 @@
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct SweavApp: App {
+    init() {
+        let appKey = (Bundle.main.infoDictionary?["KAKAO_NATIVE_APPKEY"]) as! String
+        KakaoSDK.initSDK(appKey: appKey)
+    }
+    
     var body: some Scene {
         WindowGroup {
             LoginView()
