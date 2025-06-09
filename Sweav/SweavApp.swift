f76a7e4317889649b280d16c21c38c7fb4f48229
hyunsoo <mm083434@gmail.com>
2025-06-09 18:40:14 +0900

    Initial Commit

---
+++ Sweav/SweavApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweavApp.swift
+//  Sweav
+//
+//  Created by HyunSoo on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SweavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
