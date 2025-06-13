70913be8ec7e83f2c42c474db5092d47a2d710c1
hyunsoo <mm083434@gmail.com>
2025-06-13 12:50:22 +0900

    feat: complete GoalInputView

--- Sweav/Utility/CommonUtils.swift
+++ Sweav/Utility/CommonUtils.swift
@@ -1,8 +1,10 @@
-//
-//  Common.swift
-//  Sweav
-//
-//  Created by HyunSoo on 6/13/25.
-//
-
 import Foundation
+
+struct CommonUtils {
+    static func formattedDateWithDots(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy.  M  .  d"
+        return formatter.string(from: date)
+    }
+}
--- Sweav/View/Onboarding/GoalInputView/GoalInputView.swift
+++ Sweav/View/Onboarding/GoalInputView/GoalInputView.swift
@@ -1,15 +1,149 @@
-//
-//  GoalInputView.swift
-//  Sweav
-//
-//  Created by HyunSoo on 6/13/25.
-//
-
 import SwiftUI
 
 struct GoalInputView: View {
+    @State private var showMifflinSheet: Bool = false
+    @State private var showPopover: Bool = false
+    @State private var goalDate: Date = Date()
+    @State private var goalWeight: String = ""
+    @State private var isDateChanged: Bool = false
+    @FocusState private var isGoalWeightFocused: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Button {
+                //TODO: Back
+            } label: {
+                Image("BackButton")
+            }
+            .padding(.leading, -20)
+            .padding(.vertical, 14)
+            
+            Text("목표를 정해볼까요?")
+                .font(.system(size: 24, weight: .semibold))
+                .padding(.top, 40)
+            
+            HStack(spacing: 4) {
+                Button {
+                    showMifflinSheet = true
+                } label: {
+                    HStack(spacing: 4) {
+                        Image("HelpCon")
+                        Text("Mifflin-St Jeor")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundStyle(Color.tertiary)
+                            .underline(true, pattern: .solid)
+                    }
+                }
+                .sheet(isPresented: $showMifflinSheet) {
+                    MifflinSheetView()
+                }
+                
+                Text("공식을 활용해")
+                    .font(.system(size: 14))
+                    .foregroundStyle(Color.tertiary)
+            }
+            
+            Text("적정 칼로리 및 영양분 섭취를 제안하기 위해 활용돼요.")
+                .font(.system(size: 14))
+                .foregroundStyle(Color.tertiary)
+            
+            VStack(alignment: .leading) {
+                HStack(spacing: 12) {
+                    Text("목표 날짜")
+                        .font(.system(size: 16, weight: .semibold))
+                        .foregroundStyle(Color(hex: "#BBBAB7"))
+                        .frame(width: 90, alignment: .leading)
+                    
+                    Button {
+                        showPopover = true
+                        isGoalWeightFocused = false
+                        isDateChanged = true
+                    } label: {
+                        Text("\(CommonUtils.formattedDateWithDots(goalDate))")
+                            .font(.system(size: 24, weight: .semibold))
+                            .foregroundColor(
+                                isDateChanged ? Color.default : Color(hex: "#BBBAB7"))
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 13.5)
+                    .background(Color(hex: "#FAF8F4"))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(showPopover ? Color.main : Color(hex: "#FAF8F4"), lineWidth: 4)
+                    )
+                    .cornerRadius(16)
+                    .popover(isPresented: $showPopover, attachmentAnchor: .rect(.bounds), arrowEdge: .top) {
+                        VStack {
+                            DatePicker(
+                                "",
+                                selection: $goalDate,
+                                in: Date()...Date.distantFuture,
+                                displayedComponents: [.date]
+                            )
+                            .datePickerStyle(.wheel)
+                            .labelsHidden()
+                            .padding()
+                        }
+                        .frame(width: 300)
+                        .presentationCompactAdaptation(.popover)
+                    }
+                }
+                
+                HStack(spacing: 12) {
+                    Text("목표 몸무게")
+                        .font(.system(size: 16, weight: .semibold))
+                        .foregroundStyle(Color(hex: "#BBBAB7"))
+                        .frame(width: 90, alignment: .leading)
+                    
+                    TextField(
+                        "",
+                        text: $goalWeight,
+                        prompt: Text("몸무게")
+                            .font(.system(size: 16, weight: .semibold))
+                            .foregroundColor(Color.tertiary)
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($isGoalWeightFocused)
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 24, weight: .semibold))
+                    .foregroundStyle(Color.default)
+                    .frame(width: 120)
+                    .padding(.vertical, 16)
+                    .background(Color(hex: "#FAF8F4"))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(isGoalWeightFocused ? Color.main : Color(hex: "#FAF8F4"), lineWidth: 4)
+                    )
+                    .cornerRadius(16)
+                    .onChange(of: goalWeight) { newValue in
+                        if newValue.count > 3 {
+                            goalWeight = String(goalWeight.prefix(3))
+                        }
+                    }
+                    
+                    Text("kg")
+                        .font(.system(size: 20, weight: .semibold))
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+            .padding(.top, UIScreen.main.bounds.height * 0.15)
+            
+            Spacer()
+            
+            Button {
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy-MM-dd"
+                
+                UserDefaults.standard.set(formatter.string(from: goalDate), forKey: "goalDate")
+                
+                // TODO: Navigate
+            } label: {
+                Text("Button")
+            }
+            .disabled(!isDateChanged || goalWeight.isEmpty)
+        }
+        .padding(.horizontal, 40)
+        .navigationBarBackButtonHidden(true)
     }
 }
 
